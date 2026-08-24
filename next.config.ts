a47acd7023b76a0ce4ed3c2a7e86c4690130ecf2
Yasin Eryılmaz <yasin.eryilmaz@metus.com>
2026-08-24 19:05:13 +0300

    Standalone edits

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-
 import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig = {
-  output: "standalone",
-} satisfies NextConfig;
+const nextConfig: NextConfig = {
+  // Vercel üzerinde değilken (yani Docker veya local prod build'de) standalone çıktısı üret
+  output: process.env.VERCEL ? undefined : "standalone",
+};
 
 export default withNextIntl(nextConfig);
